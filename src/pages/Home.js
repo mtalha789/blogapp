470e5298c0f4bb5b62d2bd7a92c1c4571cafee3c
Talha <mt452395@gmail.com>
2024-02-27 18:11:57 +0500

    mobile responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
             <Container>
                 <div className="flex flex-wrap">
                     {posts.map((post)=>(
-                        <div key={post.$id} className="p-2 w-1/3 ">
+                        <div key={post.$id} className=" flex flex-wrap p-2 w-full sm:w-1/3 ">
                             <PostCard {...post} />
                         </div>
                     ))}    
